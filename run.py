534d9bb8057f81d707a7d63595dcb4fc8fc17e87
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-08-28 21:46:55 +0200

    Import trivia data

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
+from trivia import sport_questions
 # LOGO
 
 
-# Question class
+#Question class
 class Question:
     """
-    Create and initialise objects for the questions class
+    Create and initialise objects for the questions class.
     """
 
     def __init__(self, question, correct_answer):
@@ -12,5 +13,13 @@ class Question:
         self.answer = correct_answer
 
 
-first_question = Question("What's your name?", "True")
-print(first_question.question)
+# first_question = Question("What's your name?", "True")
+# print(first_question.question)
+
+
+# 2. For loop to go over the  the list of trivia questions in the database
+array_of_questions = []
+"""
+The for loop will go over the questions in the database in trivia.py. For each question an object will be created and appended to the array of questions.
+"""
+for sport_questions
